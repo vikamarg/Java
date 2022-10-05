cf975c9cd71a83a6266442378931c07a749fe493
vikamarg <victoria.margaryan@gmail.com>
2022-10-05 18:09:31 +0400

    Thisrd push with git ignore

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello my beutiful world!");
         System.out.println("Im connected to GitHub with ssh!");
+        System.out.println("gitignore worked now!");
     }
 }
