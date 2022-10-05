85402df26f9814fc9c8d048dd4e7af6e754c1d4e
vikamarg <victoria.margaryan@gmail.com>
2022-10-05 17:56:14 +0400

    Second commit with ssh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello my world!");
+        System.out.println("Hello my beutiful world!");
+        System.out.println("Im connected to GitHub with ssh!");
     }
 }
